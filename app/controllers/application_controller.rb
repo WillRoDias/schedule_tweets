318082b62650dee775ee2543acbfc1baf74665e6
WillRoDias <willrodias@gmail.com>
2022-03-10 01:40:25 -0300

    Login path added and Current added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+
+    def set_current_user
+        if session[:user_id]
+            # Se houver algum dado de usuário armazenado nos cookies do navegador, armazena os dados numa variável para o front utilizar.
+            Current.user = User.find_by(id: session[:user_id])
+            # Caso não exista um usuário com esse ID que os cookies está buscando, não retorna um erro, a página será carregada em branco.
+        end
+    end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,11 +3,5 @@ class MainController < ApplicationController
     def index
         # flash.now[:notice] = "Logged in successfully"
         # flash.now[:alert] = "Invalid email or password"
-        
-        if session[:user_id]
-            # Se houver algum dado de usuário armazenado nos cookies do navegador, armazena os dados numa variável para o front utilizar.
-            @user = User.find_by(id: session[:user_id])
-            # Caso não exista um usuário com esse ID que os cookies está buscando, não retorna um erro, a página será carregada em branco.
-        end
     end    
 end
