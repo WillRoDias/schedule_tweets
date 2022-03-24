f94fda5365809f23980e754d9cf5fab0e448d525
WillRoDias <willrodias@gmail.com>
2022-03-24 01:39:48 -0300

    Reset password and list all users in index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,6 @@ class MainController < ApplicationController
     def index
         # flash.now[:notice] = "Logged in successfully"
         # flash.now[:alert] = "Invalid email or password"
+        @users = User.all
     end    
 end
